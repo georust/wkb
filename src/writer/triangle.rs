717a05c5bc0cd36847aa2291c97c6829462b5839
Kyle Barron <kyle@developmentseed.org>
2025-09-22 21:38:23 -0600

    Remove triangle wrapper

--- src/writer/triangle.rs
+++ src/writer/triangle.rs
@@ -1,62 +1,18 @@
-use std::io::Write;
-
-use geo_traits::{
-    GeometryTrait, LineStringTrait, PolygonTrait, TriangleTrait, UnimplementedGeometryCollection,
-    UnimplementedLine, UnimplementedLineString, UnimplementedMultiLineString,
-    UnimplementedMultiPoint, UnimplementedMultiPolygon, UnimplementedPoint, UnimplementedPolygon,
-    UnimplementedRect, UnimplementedTriangle,
-};
-
+use crate::common::WkbType;
 use crate::error::WkbResult;
-use crate::writer::{polygon_wkb_size, write_polygon, WriteOptions};
-
-/// A wrapper around an impl TriangleTrait to provide LineStringTrait and PolygonTrait
-struct TriangleWrapper<'a, G: TriangleTrait<T = f64>>(&'a G);
-
-impl<'a, G: TriangleTrait<T = f64>> LineStringTrait for &'a TriangleWrapper<'a, G> {
-    type CoordType<'b>
-        = G::CoordType<'a>
-    where
-        G: 'b,
-        Self: 'b;
-
-    fn num_coords(&self) -> usize {
-        3
-    }
-
-    unsafe fn coord_unchecked(&self, i: usize) -> Self::CoordType<'_> {
-        match i {
-            0 => self.0.first(),
-            1 => self.0.second(),
-            2 => self.0.third(),
-            _ => unreachable!(),
-        }
-    }
-}
-
-impl<G: TriangleTrait<T = f64>> PolygonTrait for TriangleWrapper<'_, G> {
-    type RingType<'b>
-        = &'b TriangleWrapper<'b, G>
-    where
-        G: 'b,
-        Self: 'b;
-
-    fn exterior(&self) -> Option<Self::RingType<'_>> {
-        Some(self)
-    }
-
-    fn num_interiors(&self) -> usize {
-        0
-    }
-
-    unsafe fn interior_unchecked(&self, _i: usize) -> Self::RingType<'_> {
-        unreachable!()
-    }
-}
+use crate::writer::coord::write_coord;
+use crate::writer::WriteOptions;
+use crate::Endianness;
+use byteorder::{BigEndian, ByteOrder, LittleEndian, WriteBytesExt};
+use geo_traits::TriangleTrait;
+use std::io::Write;
 
 /// The number of bytes this Triangle will take up when encoded as WKB
 pub fn triangle_wkb_size(geom: &impl TriangleTrait<T = f64>) -> usize {
-    polygon_wkb_size(&TriangleWrapper(geom))
+    let header = 1 + 4 + 4;
+    let each_coord = geom.dim().size() * 8;
+    let all_coords = 4 * each_coord;
+    header + all_coords
 }
 
 /// Write a Triangle geometry to a Writer encoded as WKB
@@ -65,137 +21,34 @@ pub fn write_triangle(
     geom: &impl TriangleTrait<T = f64>,
     options: &WriteOptions,
 ) -> WkbResult<()> {
-    write_polygon(writer, &TriangleWrapper(geom), options)
-}
+    // Byte order
+    writer.write_u8(options.endianness.into())?;
 
-impl<G: TriangleTrait<T = f64>> GeometryTrait for TriangleWrapper<'_, G> {
-    type T = f64;
-    type PointType<'b>
-        = UnimplementedPoint<f64>
-    where
-        Self: 'b;
-    type LineStringType<'b>
-        = UnimplementedLineString<f64>
-    where
-        Self: 'b;
-    type PolygonType<'b>
-        = Self
-    where
-        Self: 'b;
-    type MultiPointType<'b>
-        = UnimplementedMultiPoint<f64>
-    where
-        Self: 'b;
-    type MultiLineStringType<'b>
-        = UnimplementedMultiLineString<f64>
-    where
-        Self: 'b;
-    type MultiPolygonType<'b>
-        = UnimplementedMultiPolygon<f64>
-    where
-        Self: 'b;
-    type GeometryCollectionType<'b>
-        = UnimplementedGeometryCollection<f64>
-    where
-        Self: 'b;
-    type RectType<'b>
-        = UnimplementedRect<f64>
-    where
-        Self: 'b;
-    type LineType<'b>
-        = UnimplementedLine<f64>
-    where
-        Self: 'b;
-    type TriangleType<'b>
-        = UnimplementedTriangle<f64>
-    where
-        Self: 'b;
-
-    fn dim(&self) -> geo_traits::Dimensions {
-        geo_traits::Dimensions::Xy
-    }
-
-    fn as_type(
-        &self,
-    ) -> geo_traits::GeometryType<
-        '_,
-        Self::PointType<'_>,
-        Self::LineStringType<'_>,
-        Self::PolygonType<'_>,
-        Self::MultiPointType<'_>,
-        Self::MultiLineStringType<'_>,
-        Self::MultiPolygonType<'_>,
-        Self::GeometryCollectionType<'_>,
-        Self::RectType<'_>,
-        Self::TriangleType<'_>,
-        Self::LineType<'_>,
-    > {
-        geo_traits::GeometryType::Polygon(self)
+    // Content
+    match options.endianness {
+        Endianness::LittleEndian => write_triangle_content::<LittleEndian>(writer, geom),
+        Endianness::BigEndian => write_triangle_content::<BigEndian>(writer, geom),
     }
 }
 
-impl<'a, G: TriangleTrait<T = f64>> GeometryTrait for &'a TriangleWrapper<'a, G> {
-    type T = f64;
-    type PointType<'b>
-        = UnimplementedPoint<f64>
-    where
-        Self: 'b;
-    type LineStringType<'b>
-        = Self
-    where
-        Self: 'b;
-    type PolygonType<'b>
-        = UnimplementedPolygon<f64>
-    where
-        Self: 'b;
-    type MultiPointType<'b>
-        = UnimplementedMultiPoint<f64>
-    where
-        Self: 'b;
-    type MultiLineStringType<'b>
-        = UnimplementedMultiLineString<f64>
-    where
-        Self: 'b;
-    type MultiPolygonType<'b>
-        = UnimplementedMultiPolygon<f64>
-    where
-        Self: 'b;
-    type GeometryCollectionType<'b>
-        = UnimplementedGeometryCollection<f64>
-    where
-        Self: 'b;
-    type RectType<'b>
-        = UnimplementedRect<f64>
-    where
-        Self: 'b;
-    type LineType<'b>
-        = UnimplementedLine<f64>
-    where
-        Self: 'b;
-    type TriangleType<'b>
-        = UnimplementedTriangle<f64>
-    where
-        Self: 'b;
+fn write_triangle_content<B: ByteOrder>(
+    writer: &mut impl Write,
+    geom: &impl TriangleTrait<T = f64>,
+) -> WkbResult<()> {
+    let wkb_type = WkbType::Polygon(geom.dim().try_into()?);
+    writer.write_u32::<B>(wkb_type.into())?;
 
-    fn dim(&self) -> geo_traits::Dimensions {
-        geo_traits::Dimensions::Xy
-    }
+    // numRings
+    let num_rings = 1;
+    writer.write_u32::<B>(num_rings)?;
 
-    fn as_type(
-        &self,
-    ) -> geo_traits::GeometryType<
-        '_,
-        Self::PointType<'_>,
-        Self::LineStringType<'_>,
-        Self::PolygonType<'_>,
-        Self::MultiPointType<'_>,
-        Self::MultiLineStringType<'_>,
-        Self::MultiPolygonType<'_>,
-        Self::GeometryCollectionType<'_>,
-        Self::RectType<'_>,
-        Self::TriangleType<'_>,
-        Self::LineType<'_>,
-    > {
-        geo_traits::GeometryType::LineString(self)
-    }
+    let num_coords = 4;
+    writer.write_u32::<B>(num_coords)?;
+
+    write_coord::<B>(writer, &geom.first())?;
+    write_coord::<B>(writer, &geom.second())?;
+    write_coord::<B>(writer, &geom.third())?;
+    write_coord::<B>(writer, &geom.first())?;
+
+    Ok(())
 }
